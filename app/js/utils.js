92e3b62cba494ef0062387171e7eff02915c413c
hugo.dubs <hugo.dubs@gmail.com>
2017-06-25 09:03:39 -0400

    added utility to remove element from array

--- app/js/utils.js
+++ app/js/utils.js
@@ -53,3 +53,15 @@ function isEmpty(obj) {
 
     return true;
 }
+
+//ARRAY REMOVE ITEM
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+}
